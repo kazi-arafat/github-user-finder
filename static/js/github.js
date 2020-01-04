968f233f9bab442d72dc51215f91b488195a09cb
Kazi-arafat <arafat.kazi2448@gmail.com>
2020-01-04 20:53:05 +0530

    js file modified

--- static/js/github.js
+++ static/js/github.js
@@ -1,14 +1,9 @@
 class GitHub {
 	constructor() {
-<<<<<<< HEAD
 		this.client_id = "9e899de0e2623be755de";
 		this.client_secret = "a0f19dff267882e1a7f2b08b455468f04408958e";
 		this.repos_count = "5";
 		this.sort_by = "created_at:asc";
-=======
-		this.client_id = '9e899de0e2623be755de';
-		this.client_secret = '24d9cc74dbcebc51ecac78c97bb30c291f95fcf8';
->>>>>>> 8b6e97360602f9a32fa6feac189a3e5bae76ca3b
 	}
 
 	async getUser(user) {
@@ -27,5 +22,7 @@ class GitHub {
             profile: profileData,
             repos: repoData
 		};
+
+		
 	}
 }
